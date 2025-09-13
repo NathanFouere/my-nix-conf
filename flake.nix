a1b379a01a8302365fad4da56ecbd4b480a615f9
Fouere Nathan <nathanfouere@tutanota.com>
2025-09-13 11:17:49 +0200

    start working on separation of hosts

--- flake.nix
+++ flake.nix
@@ -13,10 +13,18 @@
   outputs = { self, nixpkgs, ... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.tour = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        ./hosts/default/configuration.nix
+        ./hosts/tour/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+    
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/laptop/configuration.nix
         inputs.home-manager.nixosModules.default
       ];
     };
