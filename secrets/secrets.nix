82aa79134fe2c173bf29c17ca67f621faba866a1
Nathan Fouéré <nathanfouere@tutanota.com>
2026-02-19 21:19:39 +0100

    update secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,18 @@
 let
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5l/UUW0KQzQpqN+04f4QiknEqFJhm1ehXNX61OPQIz nathanfouere@tutanota.com";
-  tour = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICAufs2QxpRFDJW6SGMWg0dEev+m6qO5j0liQjkxDS6H nathanfouere@tutanota.com";
+  tour = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJh56HKPIGr7O0HIjT9CFSuOAz7SIAvs5CCQDxz92nKO nathanfouere@tutanota.com";
 
-  # Ici ajouter les thinkcentre
   thinkcentre-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINFoS7IQ8mdWhwm7TDzE/FQ/Ntl+90xmUPHgozDtx59z nathanf@nixos";
   thinkcentre-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1gmWWXnCxaSEgdYK3rR9vNg9OnSzeLOJ7Cc3cDlRsJ nathanf@nixos";
-  systems = [ ];
+  systems = [
+    thinkcentre-1
+    thinkcentre-2
+  ];
 in
 {
   "tailscale.age".publicKeys = [
     laptop
     tour
-  ];
+  ]
+  ++ systems;
 }
