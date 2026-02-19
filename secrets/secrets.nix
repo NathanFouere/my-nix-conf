1c7a56627d67defd4f5d451bd6c1f863a8df9f41
Nathan Fouere <nathanfouere@tutanota.com>
2026-02-19 21:07:12 +0100

    add ssh key of thinkcenter 2

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,6 +4,7 @@ let
 
   # Ici ajouter les thinkcentre
   thinkcentre-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINFoS7IQ8mdWhwm7TDzE/FQ/Ntl+90xmUPHgozDtx59z nathanf@nixos";
+  thinkcentre-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1gmWWXnCxaSEgdYK3rR9vNg9OnSzeLOJ7Cc3cDlRsJ nathanf@nixos";
   systems = [ ];
 in
 {
